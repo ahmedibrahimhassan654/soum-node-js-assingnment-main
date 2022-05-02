7de2e198c0a8e96498dd5f94e4e5b0a652943033
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-05-02 23:01:32 +0200

    using befour each  in jest test

--- controllers/categories.js
+++ controllers/categories.js
@@ -37,7 +37,7 @@ exports.getProductCategory = asyncHandler(async (req, res, next) => {
 // @route     POST /api/v1/category
 // @access    Private
 exports.addCategory = asyncHandler(async (req, res, next) => {
-  const category = await ProductCategory.create();
+  const category = await ProductCategory.create(req.body);
 
   // res.status(200).json({
   //   success: true,
--- tests/unit/category.controller.test.js
+++ tests/unit/category.controller.test.js
@@ -1,19 +1,23 @@
 const categoryController = require("../../controllers/categories");
 const categoryModel = require("../../models/ProductCategory");
 const httpMocks = require("node-mocks-http");
+const categoryMock = require("../mock-data/newcategory.json");
 categoryModel.create = jest.fn();
+
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 
 describe("Category Controller", () => {
   it("should have add category function", () => {
     expect(typeof categoryController.addCategory).toBe("function");
   });
   it("should create category scema model ", async () => {
-    let req, res, next;
-
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null;
-    await categoryController.addCategory();
-    expect(categoryModel.create).toBeCalled();
+    req.body = categoryMock;
+    await categoryController.addCategory(req, res, next);
+    expect(categoryModel.create).toBeCalledWith(categoryMock);
   });
 });
