803a541db1cf236616f9df4320badaceca277a88
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-05-02 22:35:07 +0200

    mock request object in express api

--- tests/unit/category.controller.test.js
+++ tests/unit/category.controller.test.js
@@ -1,6 +1,6 @@
 const categoryController = require("../../controllers/categories");
 const categoryModel = require("../../models/ProductCategory");
-
+const httpMocks = require("node-mocks-http");
 categoryModel.create = jest.fn();
 
 describe("Category Controller", () => {
@@ -8,6 +8,11 @@ describe("Category Controller", () => {
     expect(typeof categoryController.addCategory).toBe("function");
   });
   it("should create category scema model ", async () => {
+    let req, res, next;
+
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
     await categoryController.addCategory();
     expect(categoryModel.create).toBeCalled();
   });
